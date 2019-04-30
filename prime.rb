5e777480911973c38ada4861cbeb1919e453532b
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-30 23:02:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+#A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole numbers that can be divided evenly into another number. The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29. Numbers that have more than two factors are called composite numbers.
+#it has to be an if/else statement because we are doing a boolean (true/false)
+
+def prime?(integer)
+if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
