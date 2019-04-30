319b5aa0a26f4a16b23eab459302b7b74244a2de
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-30 23:04:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 #A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole numbers that can be divided evenly into another number. The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29. Numbers that have more than two factors are called composite numbers.
 #it has to be an if/else statement because we are doing a boolean (true/false)
 
-def prime?(integer)
+def prime?(num)
 if num < 0 or num == 0 or num == 1
     return false
   else
