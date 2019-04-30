2785ed28598229c0010bb81d8edd24c8d1a566b1
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-30 23:08:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -4,11 +4,15 @@
 #it has to be an if/else statement because we are doing a boolean (true/false)
 
 def prime?(num)
-if num < 0 or num == 0 or num == 1
+#negative numbers, 0, and 1 are automatically not prime numbers so we can automatically say they are false
+
+if num < 0 || num == 0 || num == 1
     return false
+
+    
   else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    (2..num-1).to_a.all? do |prime|
+      num % prime != 0
     end
   end
 end
