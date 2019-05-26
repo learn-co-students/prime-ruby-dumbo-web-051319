f004b6a04a56265564549b31eafe2aa7bcb27ad6
L.R.L <DaleLoreNY@gmail.com>
2019-05-25 22:26:07 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,5 @@
 # Add  code here!
 
-#A prime number is a whole number greater than 1 whose only factors are 1 and itself. A factor is a whole numbers that can be divided evenly into another number. The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29. Numbers that have more than two factors are called composite numbers.
-#it has to be an if/else statement because we are doing a boolean (true/false)
-
 def prime?(num)
   
   if num < 0 || num == 0 || num == 1
@@ -14,16 +11,22 @@ def prime?(num)
   end 
 end
 
-#First need to determine range! i.e. Error: "Be sure to account for negative numbers!"
+#Need to determine range! i.e. Error: "Be sure to account for negative numbers!"
        #Be sure to account for negative numbers!
        #primes cannot be negative numbers
        #primes cannot be 0
        #primes cannot be 1
        #since it cannot be 1, the range must start at 2
-       #.none? method returns true if the block never returns true for all elements. 
+#Using .none? method returns true if the block never returns true for all elements. 
        #If the block is not given, none? will return true only if none of the collection members is true.
        #So if (num) given is a prime number it will never equal 
+#NOTE: The code HAS to be an if/else statement because we are doing a boolean (true/false)
 
 
-
-       #The prime algorithm is explained well in this video https://www.youtube.com/watch?v=33pLqGvk-PM
+      #The prime algorithm is explained well in this video https://www.youtube.com/watch?v=33pLqGvk-PM
+      #WTF is a prime number?????
+      #A prime number is a whole number greater than 1 whose only factors are 1 and itself. 
+      #A factor is a whole number that can be divided evenly into another number. 
+      #The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29. 
+      #Numbers that have more than two factors are called composite numbers.  
+      
